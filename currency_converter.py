48ab1a484454dfe859fb31879926a253b22755ec
Kik <tina.miklasova@gmail.com>
2017-06-17 13:06:10 +0200

    Creating exchange rate as dictionary

--- currency_converter.py
+++ currency_converter.py
@@ -1,25 +1,31 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+
+def create_exchange_rate():
+    exchange_rate = {}
+    for row in currency_table.findAll("tr"):
+        cells = row.findAll("td")
+        if len(cells) == 5:
+            value = cells[2].find(text=True).strip("\r\n\t ").replace("\xa0", "").replace(",", ".")
+            currency = cells[0].find(text=True).strip(" ")
+            try:
+                float_value = float(value)
+                exchange_rate[currency] = float_value
+            except ValueError:
+                # print(currency + " - no value")
+                continue
+
+    return exchange_rate
+
+
 with urllib.request.urlopen("http://www.nbs.sk/sk/statisticke-udaje/kurzovy-listok/denny-kurzovy-listok-ecb") as url:
     page = url.read()
 
 soup = BeautifulSoup(page, "lxml")
-#print(soup.prettify())
+# print(soup.prettify())
 trs = soup.find_all("tr")
 currency_table = soup.find('table', class_='tblBorder')
 
-currency = []
-currency_code = []
-value = []
-
-for row in currency_table.findAll("tr"):
-    cells = row.findAll("td")
-    if len(cells) == 5:
-        currency_code.append(cells[0].find(text=True))
-        currency.append(cells[1].find(text=True))
-        value.append(cells[2].find(text=True))
-
-print(currency_code)
-print(currency)
-print(value)
+exchange_rate = create_exchange_rate()
+print(exchange_rate)
