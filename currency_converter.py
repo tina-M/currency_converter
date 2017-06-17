93f10160e58446e7bee1ae8a84dc2d1c53908ebc
Kik <tina.miklasova@gmail.com>
2017-06-17 14:10:41 +0200

    Adding the dictionary of currency symbols

--- currency_converter.py
+++ currency_converter.py
@@ -3,6 +3,11 @@ import getopt
 import urllib.request
 from bs4 import BeautifulSoup
 
+CURRENCY_SYMBOLS = {'HUF': 'Ft', 'MYR': 'RM', 'JPY': '¥', 'USD': '$', 'SEK': 'kr', 'KRW': '₩', 'NZD': '$', 'CHF': 'CHF',
+                    'SGD': '$', 'RON': 'lei', 'CNY': '¥', 'CZK': 'Kč', 'BRL': 'R$', 'RUB': '₽', 'BGN': 'лв', 'ILS': '₪',
+                    'MXN': '$', 'THB': '฿', 'HRK': 'kn', 'GBP': '£', 'DKK': 'kr', 'TRY': '₺', 'CAD': '$', 'NOK': 'kr',
+                    'ZAR': 'R', 'HKD': 'HK$', 'AUD': '$', 'INR': '₹', 'PLN': 'zł', 'IDR': 'Rp', 'PHP': '₱'}
+
 
 # access to web page of the Central bank of the Slovak Republic and load content - the table of exchange rate
 def scrape_web_page():
@@ -18,6 +23,7 @@ def scrape_web_page():
 
 # from the table of exchange rate create the dictionary - <currency> : <amount>
 def create_exchange_rate():
+    cur = []
     currency_table = scrape_web_page()
     exchange_rate = {}
     for row in currency_table.findAll("tr"):
@@ -27,11 +33,13 @@ def create_exchange_rate():
             currency = cells[0].find(text=True).strip(" ")
             try:
                 float_value = float(value)
+                cur.append(currency)
                 exchange_rate[currency] = float_value
             except ValueError:
                 # print(currency + " - no value")
                 continue
 
+    print(cur)
     return exchange_rate
 
 
@@ -57,4 +65,4 @@ print(input_currency)
 print(output_currency)
 
 exchange_rate = create_exchange_rate()
-#print(exchange_rate)
+print(exchange_rate)
